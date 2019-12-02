e3b279ebb962d9047b2060345176542f9d223663
Tonyxu74 <tonyxu74@hotmail.com>
2019-12-01 16:06:28 -0800

    added build vocab

--- utils/predict.py
+++ utils/predict.py
@@ -1,8 +1,8 @@
-from utils.dataset import GenerateIter
 from torchtext import data
 from utils.model import LSTMAttention
-from torch import optim
-from torch import nn
+import pandas as pd
+from opts import args
+from torchtext.vocab import GloVe
 import torch
 
 
@@ -35,9 +35,23 @@ def predict(string, epoch):
     print(pred_class)
 
 
-def build_vocab():
-    print("todo")
+def build_vocab(path):
+
+    dataframe = pd.read_csv(path, sep="\t", encoding="utf-8")
+    text_field = data.Field(lower=True, include_lengths=True, batch_first=True, preprocessing=append_endtag)
+    dataset = []
+
+    for i in range(dataframe.shape[0]):
+        dataset.append({
+            'text': text_field.preprocess(dataframe['text'][i]),
+            'label': dataframe['label'][i]
+        }
+    )
+
+    text_field.build_vocab([t['text'] for t in dataset], vectors=GloVe(name='6B', dim=args.embedding_dims))
+    torch.save(text_field, 'vocab_field.pt')
 
 
 if __name__ == "__main__":
-    predict('This movie is bad.', epoch=10)
+    # build_vocab('../sentiment_data/train.csv')
+    predict("The actors was brilliant, and the story was quite good.", epoch=25)
